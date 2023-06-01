701e33bf9edb6a249ab0241321a2d357db999b67
abihaladha456 <119162098+abihaladha456@users.noreply.github.com>
2023-06-01 19:55:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera= document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captureimg" src="'+data_uri+'"</img>';
+    })
+}
+console.log("ml5 version" , ml5.version);
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/5fm9FHprX/model.json", modelLoaded);
+function modelLoaded(){
+console.log("modelLoaded");
+}
+
+function check(){
+    img = document.getElementById("captureimg");
+    classifier.classify(img, gotresult);
+}
+
+function gotresult(error,results){
+    if (error){
+        console.log(error);
+    }else {
+        console.log(results);
+        document.getElementById("resultobjectname").innerHTML=results[0].label;
+       document.getElementById("resultobjectaccuracy").innerHTML=results[0].confidence.toFixed(3);
+
+    }
+}
